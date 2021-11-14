1c4e79333e7c5dcaf837142250c06e63b9b3c8e5
Dafei Du <dud@oregonstate.edu>
2021-11-13 21:45:29 -0500

    Create Home page and Confirm Page

---
+++ Group3/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Group3
+//
+//  Created by Dafei Du on 11/13/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
